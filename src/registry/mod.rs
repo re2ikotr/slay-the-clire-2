5dc6c33396eaa596408a7cdcf350fba92a90ec82
re2ikotr <re3ikotr@gmail.com>
2026-05-16 21:30:19 +0800

    Simplify definition registry

--- src/registry/mod.rs
+++ src/registry/mod.rs
@@ -1,20 +1,106 @@
-pub mod cards;
-pub mod monsters;
-pub mod potions;
-pub mod powers;
-pub mod relics;
+use std::collections::BTreeMap;
 
-pub use cards::CardRegistry;
-pub use monsters::MonsterRegistry;
-pub use potions::PotionRegistry;
-pub use powers::PowerRegistry;
-pub use relics::RelicRegistry;
+use crate::content::cards::CardDef;
+use crate::content::monsters::MonsterDef;
+use crate::content::potions::PotionDef;
+use crate::content::powers::PowerDef;
+use crate::content::relics::RelicDef;
+use crate::core::ids::{CardId, MonsterId, PotionId, PowerId, RelicId};
+
+pub trait RegistryDef {
+    type Id: Copy + Ord;
+
+    fn registry_id(&self) -> Self::Id;
+}
+
+pub struct DefRegistry<Id, Def> {
+    defs: BTreeMap<Id, Def>,
+}
+
+impl<Id, Def> Default for DefRegistry<Id, Def> {
+    fn default() -> Self {
+        Self {
+            defs: BTreeMap::new(),
+        }
+    }
+}
+
+impl<Id, Def> DefRegistry<Id, Def>
+where
+    Id: Copy + Ord,
+    Def: RegistryDef<Id = Id>,
+{
+    pub fn register(&mut self, def: Def) {
+        self.defs.insert(def.registry_id(), def);
+    }
+}
+
+impl<Id, Def> DefRegistry<Id, Def>
+where
+    Id: Copy + Ord,
+{
+    pub fn get(&self, id: Id) -> Option<&Def> {
+        self.defs.get(&id)
+    }
+
+    pub fn contains(&self, id: Id) -> bool {
+        self.defs.contains_key(&id)
+    }
+
+    pub fn len(&self) -> usize {
+        self.defs.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.defs.is_empty()
+    }
+}
+
+impl RegistryDef for CardDef {
+    type Id = CardId;
+
+    fn registry_id(&self) -> Self::Id {
+        self.id
+    }
+}
+
+impl RegistryDef for PowerDef {
+    type Id = PowerId;
+
+    fn registry_id(&self) -> Self::Id {
+        self.id
+    }
+}
+
+impl RegistryDef for RelicDef {
+    type Id = RelicId;
+
+    fn registry_id(&self) -> Self::Id {
+        self.id
+    }
+}
+
+impl RegistryDef for PotionDef {
+    type Id = PotionId;
+
+    fn registry_id(&self) -> Self::Id {
+        self.id
+    }
+}
+
+impl RegistryDef for MonsterDef {
+    type Id = MonsterId;
+
+    fn registry_id(&self) -> Self::Id {
+        self.id
+    }
+}
 
 #[derive(Default)]
 pub struct StaticRegistry {
-    pub cards: CardRegistry,
-    pub powers: PowerRegistry,
-    pub relics: RelicRegistry,
-    pub potions: PotionRegistry,
-    pub monsters: MonsterRegistry,
+    pub cards: DefRegistry<CardId, CardDef>,
+    pub powers: DefRegistry<PowerId, PowerDef>,
+    pub relics: DefRegistry<RelicId, RelicDef>,
+    pub potions: DefRegistry<PotionId, PotionDef>,
+    pub monsters: DefRegistry<MonsterId, MonsterDef>,
 }
